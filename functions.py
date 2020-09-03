0e100b00092a73ad447549751ff8c1d5b0003ce4
Ben Chan <benchan777@gmail.com>
2020-09-03 00:19:41 -0700

    update

--- functions.py
+++ functions.py
@@ -1,16 +1,29 @@
+#Answer for Q1
 def area(width, height):
     result = width * height
     return result
+#Answer for Q2
 result = area(5, 6)
 print(result)
+#Answers for Q3
 result = area(9, 12)
 print(result)
 result = area(1.5, 2.2)
 print(result)
+#Answer for Q4
 def subtract(num1, num2):
     result = num1 - num2
     return result
+#Answers for Q5
 result = subtract(90, 80)
 print(result)
 result = subtract(43, 91)
 print(result)
+#Answer for Q7
+def add(num1, num2):
+    result = num1 + num2
+    return result
+addition_result = add(87, 21)
+print(addition_result)
+addition_result = add(2.3, 87.44)
+print(addition_result)
