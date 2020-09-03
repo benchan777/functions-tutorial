e46e087177bfdaff3121685c112e62cd692a2567
Ben Chan <benchan777@gmail.com>
2020-09-03 00:03:55 -0700

    update

---
+++ functions.py
@@ -0,0 +1,16 @@
+def area(width, height):
+    result = width * height
+    return result
+result = area(5, 6)
+print(result)
+result = area(9, 12)
+print(result)
+result = area(1.5, 2.2)
+print(result)
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+result = subtract(90, 80)
+print(result)
+result = subtract(43, 91)
+print(result)
